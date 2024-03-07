860a2b69c6d5da00fd5339c7755d7bfd63b4bdc8
sallieeky <sallieeky@gmail.com>
2024-03-07 13:40:52 +0700

    init

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Pkt\StarterKit\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Symfony\Component\Process\Process;
+
+
+class InstallCommand extends Command implements PromptsForMissingInput
+{
+    use InstallBladeStack, InstallVueStack, InstallReactStack;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pkt:install 
+                            {stack : The development stack that should be installed (blade, vue, react)}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the Pkt Starter Kit into the application.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int|null
+     */
+    public function handle()
+    {
+        if ($this->argument('stack') === 'blade') {
+            $this->installBladeStack();
+        } elseif ($this->argument('stack') === 'vue') {
+            $this->installVueStack();
+        } elseif ($this->argument('stack') === 'react') {
+            $this->installReactStack();
+        } else {
+            $this->error('Invalid stack. Please use "blade", "vue", or "react".');
+            return 1;
+        }
+        return 1;
+    }
+
+    /**
+     * Update the "package.json" file.
+     *
+     * @param  callable  $callback
+     * @param  bool  $dev
+     * @return void
+     */
+    protected static function updateNodePackages(callable $callback, $dev = true)
+    {
+        if (! file_exists(self::base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages[$configurationKey] = $callback(
+            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
+            $configurationKey
+        );
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
+        );
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @return void
+     */
+    protected function runCommands($commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (\RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
+    }
+}
---
+++ src/Console/InstallReactStack.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Pkt\StarterKit\Console;
+
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+trait InstallReactStack
+{
+    protected function installReactStack()
+    {
+        $this->components->info('Installing Blade stack...');
+
+        // call breeze:install react
+        $this->callSilent('breeze:install', ['react']);
+
+        // // Controllers...
+        // (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
+        // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/blade/app/Http/Controllers', app_path('Http/Controllers'));
+
+        // // Views...
+        // (new Filesystem)->ensureDirectoryExists(resource_path('views'));
+        // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/blade/resources/views', resource_path('views'));
+
+        // // Routes...
+        // copy(__DIR__.'/../../stubs/blade/routes/web.php', base_path('routes/web.php'));
+
+        // // Tailwind / Vite...
+        // copy(__DIR__.'/../../stubs/blade/tailwind.config.js', base_path('tailwind.config.js'));
+        // copy(__DIR__.'/../../stubs/blade/postcss.config.js', base_path('postcss.config.js'));
+        // copy(__DIR__.'/../../stubs/blade/vite.config.js', base_path('vite.config.js'));
+        // copy(__DIR__.'/../../stubs/blade/resources/css/app.css', resource_path('css/app.css'));
+        // copy(__DIR__.'/../../stubs/blade/resources/js/app.js', resource_path('js/app.js'));
+
+        // $this->components->info('Installing and building Node dependencies.');
+
+        // if (file_exists(base_path('pnpm-lock.yaml'))) {
+        //     $this->runCommands(['pnpm install', 'pnpm run build']);
+        // } elseif (file_exists(base_path('yarn.lock'))) {
+        //     $this->runCommands(['yarn install', 'yarn run build']);
+        // } else {
+        //     $this->runCommands(['npm install', 'npm run build']);
+        // }
+
+        // $this->line('');
+        // $this->components->info('Eky scaffolding installed successfully.');
+    }
+}
