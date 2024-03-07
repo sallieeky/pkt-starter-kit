3f7940df15973e040ebbc8b9ffa5bfd4403cc3e5
sallieeky <sallieeky@gmail.com>
2024-03-07 20:52:37 +0800

    🔧 chore(InstallCommand.php): refactor requireComposerPackages method for better readability and maintainability

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -129,4 +129,32 @@ class InstallCommand extends Command implements PromptsForMissingInput
             $this->output->write('    '.$line);
         });
     }
+
+    /**
+     * Installs the given Composer Packages into the application.
+     *
+     * @param  array  $packages
+     * @param  bool  $asDev
+     * @return bool
+     */
+    protected function requireComposerPackages(array $packages, $asDev = false)
+    {
+        $composer = $this->option('composer');
+
+        if ($composer !== 'global') {
+            $command = ['php', $composer, 'require'];
+        }
+
+        $command = array_merge(
+            $command ?? ['composer', 'require'],
+            $packages,
+            $asDev ? ['--dev'] : [],
+        );
+
+        return (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']))
+            ->setTimeout(null)
+            ->run(function ($type, $output) {
+                $this->output->write($output);
+            }) === 0;
+    }
 }
