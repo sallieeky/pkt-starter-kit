e6de6d967bae305ecb147c6d4f89b2c24dbde814
sallieeky <sallieeky@gmail.com>
2024-03-07 19:28:30 +0800

    🔧 chore(InstallReactStack.php): clean unnecessary files from breeze to improve project structure and remove unused code 🔧 chore(InstallReactStack.php): update the "package.json" file to include necessary dependencies for React stack 🔧 chore(InstallReactStack.php): create controllers for React stack to handle backend logic 🔧 chore(InstallReactStack.php): create views/pages for React stack to display frontend UI 🔧 chore(InstallReactStack.php): create routes for React stack to define API endpoints 🔧 chore(InstallReactStack.php): install new npm module and build to update project dependencies and generate necessary files

--- src/Console/InstallReactStack.php
+++ src/Console/InstallReactStack.php
@@ -17,13 +17,16 @@ trait InstallReactStack
         // End call breeze
 
         // Clean unnecessary files from breeze
+        $this->components->info('Cleaning unnecessary files from breeze...');
         (new Filesystem)->ensureDirectoryExists(resource_path('js/Components'));
         (new Filesystem)->ensureDirectoryExists(resource_path('js/Layouts'));
         (new Filesystem)->deleteDirectory(resource_path('js/Components'));
         (new Filesystem)->deleteDirectory(resource_path('js/Layouts'));
+        $this->components->success('Unnecessary files cleaned.');
         // End clean unnecessary files from breeze
 
         // Update the "package.json" file
+        $this->components->info('Updating the "package.json" file...');
         $this->updateNodePackages(function ($packages) {
             return [
                 // example
@@ -31,28 +34,36 @@ trait InstallReactStack
                 // ...
                 ] + $packages;
         });
+        $this->components->success('The "package.json" file updated.');
         // End update the "package.json" file
 
         // Controllers
+        $this->components->info('Creating controllers...');
         (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/vue/app/Http/Controllers', app_path('Http/Controllers'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/app/Http/Controllers', app_path('Http/Controllers'));
+        $this->components->success('Controllers created.');
         // End controllers
 
         // Views/Pages
+        $this->components->info('Creating views/pages...');
         (new Filesystem)->ensureDirectoryExists(resource_path('views'));
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/vue/resources/views', resource_path('views'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/resources/views', resource_path('views'));
+        $this->components->success('Views/pages created.');
         // End views/Pages
 
         // Routes
-        copy(__DIR__.'/../../stubs/vue/routes/web.php', base_path('routes/web.php'));
+        $this->components->info('Creating routes...');
+        copy(__DIR__.'/../../stubs/react/routes/web.php', base_path('routes/web.php'));
+        $this->components->success('Routes created.');
         // End routes
 
         // Update npm packages
+        $this->components->info('Installing new npm module and build...');
         $this->runCommands(['npm install', 'npm run build']);
+        $this->components->success('Npm module installed.');
         // End update npm packages
 
         $this->line('');
-
         $this->components->info('PKT Starter Kit installed with React stack.');
     }
 }
