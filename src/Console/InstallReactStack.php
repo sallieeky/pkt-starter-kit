d0023b716113da8da01415784c45673d5a995afc
sallieeky <sallieeky@gmail.com>
2024-03-07 20:21:35 +0800

    🔧 fix(InstallReactStack.php): refactor code to use task() method for cleaner output and better readability ✨ feat(InstallReactStack.php): add support for updating the "package.json" file and creating controllers, views/pages, and routes 🔧 fix(InstallVueStack.php): refactor code to use task() method for cleaner output and better readability ✨ feat(InstallVueStack.php): add support for updating the "package.json" file and creating controllers, views/pages, and routes

--- src/Console/InstallReactStack.php
+++ src/Console/InstallReactStack.php
@@ -17,56 +17,63 @@ trait InstallReactStack
         // End call breeze
 
         // Clean unnecessary files from breeze
-        $this->components->info('Cleaning unnecessary files from breeze...');
-        (new Filesystem)->ensureDirectoryExists(resource_path('js/Components'));
-        (new Filesystem)->ensureDirectoryExists(resource_path('js/Layouts'));
-        (new Filesystem)->deleteDirectory(resource_path('js/Components'));
-        (new Filesystem)->deleteDirectory(resource_path('js/Layouts'));
-        $this->components->info('Unnecessary files cleaned.');
+        $this->components->task('Cleaning unnecessary files from breeze...', function () {
+            (new Filesystem)->ensureDirectoryExists(resource_path('js/Components'));
+            (new Filesystem)->ensureDirectoryExists(resource_path('js/Layouts'));
+            (new Filesystem)->deleteDirectory(resource_path('js/Components'));
+            (new Filesystem)->deleteDirectory(resource_path('js/Layouts'));
+        });
+        $this->info('Unnecessary files cleaned.');
         // End clean unnecessary files from breeze
 
         // Update the "package.json" file
-        $this->components->info('Updating the "package.json" file...');
-        $this->updateNodePackages(function ($packages) {
-            return [
-                // example
-                // 'axios' => '^0.21',
-                // ...
-                ] + $packages;
+        $this->components->task('Updating the "package.json" file...', function () {
+            $this->updateNodePackages(function ($packages) {
+                return [
+                    // example
+                    // 'axios' => '^0.21',
+                    // ...
+                    ] + $packages;
+            });
         });
-        $this->components->info('The "package.json" file updated.');
+        $this->info('The "package.json" file updated.');
         // End update the "package.json" file
 
         // Controllers
-        $this->components->info('Creating controllers...');
-        // (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
-        // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/app/Http/Controllers', app_path('Http/Controllers'));
-        $this->components->info('Controllers created.');
+        $this->components->task('Creating controllers...', function () {
+            // (new Filesystem)->ensureDirectoryExists(app_path('Http/Controllers'));
+            // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/app/Http/Controllers', app_path('Http/Controllers'));
+        });
+        $this->info('Controllers created.');
         // End controllers
 
         // Views/Pages
-        $this->components->info('Creating views/pages...');
-        // (new Filesystem)->ensureDirectoryExists(resource_path('views'));
-        // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/resources/views', resource_path('views'));
-        $this->components->info('Views/pages created.');
+        $this->components->task('Creating views/pages...', function () {
+            // (new Filesystem)->ensureDirectoryExists(resource_path('views'));
+            // (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/react/resources/views', resource_path('views'));
+        });
+        $this->info('Views/pages created.');
         // End views/Pages
 
         // Routes
-        $this->components->info('Creating routes...');
-        // copy(__DIR__.'/../../stubs/react/routes/web.php', base_path('routes/web.php'));
-        $this->components->info('Routes created.');
+        $this->components->task('Creating routes...', function () {
+            // copy(__DIR__.'/../../stubs/react/routes/web.php', base_path('routes/web.php'));
+        });
+        $this->info('Routes created.');
         // End routes
 
         // Update npm packages
-        $this->components->info('Installing new npm module and build...');
-        $this->runCommands(['npm install', 'npm run build']);
-        $this->components->info('Npm module installed.');
+        $this->components->task('Installing new npm module and build...', function () {
+            $this->runCommands(['npm install', 'npm run build']);
+        });
+        $this->info('Npm module installed.');
         // End update npm packages
 
         // Copy default
-        $this->components->info('Copying default template...');
-        $this->copyDefault();
-        $this->components->info('Default template copied.');
+        $this->components->task('Copying default template...', function () {
+            $this->copyDefault();
+        });
+        $this->info('Default template copied.');
         // End copy default
 
         $this->line('');
