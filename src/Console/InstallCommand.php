d5069df3e105f27ff2a3030c4d25f715ee012ab9
sallieeky <sallieeky@gmail.com>
2024-03-07 20:58:27 +0800

    🔧 chore(InstallCommand.php): add method `replaceInFile` to improve code reusability and readability

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -157,4 +157,17 @@ class InstallCommand extends Command implements PromptsForMissingInput
                 $this->output->write($output);
             }) === 0;
     }
+
+    /**
+     * Replace a given string within a given file.
+     *
+     * @param  string  $search
+     * @param  string  $replace
+     * @param  string  $path
+     * @return void
+     */
+    protected function replaceInFile($search, $replace, $path)
+    {
+        file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
+    }
 }
