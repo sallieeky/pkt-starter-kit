8162e5d75c581da1c1485063e806071bc207294d
sallieeky <sallieeky@gmail.com>
2024-04-19 09:44:20 +0800

    📝 docs(GUIDE.md): add development guide documentation for managing users, permissions, and pages 🐛 fix(InstallVueStack.php): fix import statement in app.css file to correctly import vendor styles 🐛 fix(permission-check.js): add support for checking permissions as an array

--- stubs/default/resources/js/Core/Helpers/permission-check.js
+++ stubs/default/resources/js/Core/Helpers/permission-check.js
@@ -1,10 +1,12 @@
 import { usePage } from "@inertiajs/vue3";
 
 export function can(permission){
-    if(permission){
+    if (typeof permission === 'string') {
         var permissionArray = permission.split('|');
         return permissionArray.reduce((res, permissionCursor) => res || usePage().props.auth.user_permissions.includes(permissionCursor.trim()), false);
-    }else{
+    } else if (Array.isArray(permission)){
+        return permission.reduce((res, permissionCursor) => res || usePage().props.auth.user_permissions.includes(permissionCursor.trim()), false);
+    } else{
         return true;
     }
 }
