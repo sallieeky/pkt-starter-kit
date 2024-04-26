b690275bd577dcff5f8a1c325b562ea90aa51343
sallieeky <sallieeky@gmail.com>
2024-04-26 10:42:52 +0800

    🔧 refactor(LeaderApi.php): remove commented out response examples to improve code readability and maintainability

--- src/Helpers/LeaderApi.php
+++ src/Helpers/LeaderApi.php
@@ -31,30 +31,6 @@ class LeaderApi
             'Api-Key' => env('LEADER_API_KEY')
         ])->get('https://leader.pupukkaltim.com/api/Api_leader/get_all_karyawan');
 
-        // {
-        //   "code": 1,
-        //   "message": "Success",
-        //   "data": {
-        //       "API_REQUEST": "GET ALL KARYAWAN",
-        //       "USERS_COUNT": 3968,
-        //       "USERS_LIST": [
-        //           {
-        //               "USERS_NPK": "4882980",
-        //               "USERS_EMAIL": "4882980@pupukkaltim.com",
-        //               "USERS_NAME": "Hadi Kasiono",
-        //               "USERS_USERNAME": "4882980",
-        //               "USERS_HIERARCHY_CODE": "12100000LCALFN",
-        //               "USERS_ID_POSISI": "50048724",
-        //               "USERS_POSISI": "Staf PBP",
-        //               "USERS_ID_UNIT_KERJA": "D002100000",
-        //               "USERS_UNIT_KERJA": "Kompartemen Operasi I",
-        //               "USERS_FLAG": "TKO"
-        //           },
-        //           ...
-        //       ]
-        //   }
-        // }
-
         return collect($response->object()->data->USERS_LIST);
     }
 
@@ -75,24 +51,6 @@ class LeaderApi
             'Api-Key' => env('LEADER_API_KEY')
         ])->get('https://leader.pupukkaltim.com/api/Api_leader/get_all_unit_kerja');
         
-        // {
-        //   "code": 1,
-        //   "message": "Success",
-        //   "data": {
-        //       "API_REQUEST": "GET ALL UNIT KERJA",
-        //       "UK_COUNT": 176,
-        //       "UK_LIST": [
-        //           {
-        //               "KodeUnitKerja": "D001000000",
-        //               "UnitKerja": "Direktorat Utama",
-        //               "Keterangan": "1",
-        //               "IsAktif": 1
-        //           },
-        //           ...
-        //       ]
-        //   }
-        // }
-
         return collect($response->object()->data->UK_LIST);
     }
 
@@ -112,24 +70,6 @@ class LeaderApi
         $response = Http::withHeaders([
             'Api-Key' => env('LEADER_API_KEY')
         ])->get('https://leader.pupukkaltim.com/api/Api_leader/get_all_plt');
-
-        // {
-        //   "code": 1,
-        //   "message": "Success",
-        //   "data": {
-        //       "API_REQUEST": "GET ALL PLT",
-        //       "PLT_COUNT": 31,
-        //       "PLT_LIST": [
-        //           {
-        //               "PERSONNEL_NUMBER": "4063592",
-        //               "VALID_FROM": "2024-03-04",
-        //               "VALID_TO": "2024-04-30",
-        //               "NAMA_POSISI": "VP Hukum"
-        //           },
-        //           ...
-        //       ]
-        //   }
-        // }
 
         return collect($response->object()->data->PLT_LIST);
     }
