1ac164a3084fa3d7453af13c2304838ab5d42925
sallieeky <sallieeky@gmail.com>
2024-05-20 10:26:16 +0800

    📦 chore(Model.php): add ModelName model file to database-table-stubs/Models directory 📦 chore(migration.php): add migration file for creating TableNames table to database-table-stubs/migrations directory 📦 chore(MakeDatabaseTableCommand.php): add MakeDatabaseTableCommand file to src/Console/MakeDatabaseTableCommand directory

---
+++ database-table-stubs/Models/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ModelName extends Model
+{
+    use HasFactory, HasUuids, SoftDeletes;
+    protected $guarded = ['table_name_id', 'table_name_uuid'];
+
+    /**
+     * Get the columns that should receive a unique identifier.
+     *
+     * @return array<int, string>
+     */
+    public function uniqueIds(): array
+    {
+        return ['table_name_uuid'];
+    }
+}
