86cbc9b2205745fc03c472421431ab6171c54a24
sallieeky <sallieeky@gmail.com>
2024-05-21 16:26:41 +0800

    🐛 fix(MakeResourceCommand.php): fix typo in directory path for creating test cases ✨ feat(MakeResourceCommand.php): add check for existing test cases and provide option to force overwrite

--- src/Console/MakeResourceCommand/MakeResourceCommand.php
+++ src/Console/MakeResourceCommand/MakeResourceCommand.php
@@ -77,15 +77,13 @@ class MakeResourceCommand extends Command implements PromptsForMissingInput
      */
     protected function createTestCases($model, $nameArgument)
     {
-        // cek apakah folder tests ada
         $exist = (new Filesystem)->exists(base_path('tests/Feature/'.$nameArgument));
         if ($exist && !$this->option('force')) {
             $this->error('Test already exists: tests/Feature/'.$nameArgument);
             return 0;
         }
 
-        // create folder tests/Feature/ModelName
-        (new Filesystem)->ensureDirectoryExists(base_path('tests/Feature'.$nameArgument));
+        (new Filesystem)->ensureDirectoryExists(base_path('tests/Feature/'.$nameArgument));
         copy(__DIR__.'/../../../resource-template/default/tests/BrowseTest.php', base_path('tests/Feature/'.$nameArgument.'/Browse'.$nameArgument.'Test.php'));
         copy(__DIR__.'/../../../resource-template/default/tests/CreateTest.php', base_path('tests/Feature/'.$nameArgument.'/Create'.$nameArgument.'Test.php'));
         copy(__DIR__.'/../../../resource-template/default/tests/UpdateTest.php', base_path('tests/Feature/'.$nameArgument.'/Update'.$nameArgument.'Test.php'));
