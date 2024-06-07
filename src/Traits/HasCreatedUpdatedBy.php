65c725e31b2979df20a340c07275f51643d519b0
sallieeky <sallieeky@gmail.com>
2024-06-07 17:17:02 +0800

    📝 docs(GUIDE.md): add documentation for implementing created and updated by feature
    🔧 fix(StarterKitServiceProvider.php): add macro to Blueprint for creating created_by and updated_by columns
    
    ✨ feat(HasCreatedUpdatedBy.php): add trait for implementing created and updated by feature in models

---
+++ src/Traits/HasCreatedUpdatedBy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Pkt\StarterKit\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait HasCreatedUpdatedBy
+{
+    /**
+     * The "booting" method of the trait.
+     *
+     * @return void
+     */
+    public static function bootHasCreatedUpdatedBy(): void
+    {
+        static::creating(function (Model $model) {
+            $model->created_by = auth()->user()?->{auth()->user()?->getKeyName()};
+            $model->updated_by = auth()->user()?->{auth()->user()?->getKeyName()};
+        });
+
+        static::updating(function (Model $model) {
+            $model->updated_by = auth()->user()?->{auth()->user()?->getKeyName()};
+        });
+    }
+
+    /**
+     * Get the user that created the model.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(config('auth.providers.users.model'), 'created_by', auth()->user()?->getKeyName());
+    }
+
+    /**
+     * Get the user that updated the model.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function updatedBy(): BelongsTo
+    {
+        return $this->belongsTo(config('auth.providers.users.model'), 'updated_by', auth()->user()?->getKeyName());
+    }
+}
+
