9a90139dd29de20f6a2ff6c99de1d4a51e60de43
sallieeky <sallieeky@gmail.com>
2024-06-13 15:02:18 +0800

    🐛 fix(EloquentBuilderMacro.php): remove commented out code and fix indentation for better readability 🐛 fix(QueryBuilderMacro.php): remove commented out code and fix indentation for better readability 🐛 fix(StarterKitMacroServiceProvider.php): fix import statement for Builder class 🐛 fix(HasCreatedUpdatedBy.php): remove withTrashed() method call in createdBy() and updatedBy() methods

--- src/Macros/EloquentBuilderMacro.php
+++ src/Macros/EloquentBuilderMacro.php
@@ -3,6 +3,7 @@
 namespace Pkt\StarterKit\Macros;
 
 use Illuminate\Database\Eloquent\Builder;
+use Pkt\StarterKit\Helpers\Crypt;
 
 /**
  * Class EloquentBuilderMacro for adding custom macros to the Eloquent Builder class.
@@ -21,124 +22,147 @@ use Illuminate\Database\Eloquent\Builder;
  */
 class EloquentBuilderMacro
 {
-    // /**
-    //  * where encrypted clause query to support encrypted columns
-    //  *
-    //  * @param  string $column
-    //  * @param  mixed  $operator = null
-    //  * @param  mixed  $value = null
-    //  * @param  string  $boolean = 'and'
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('whereEncrypted', function (string $column, $operator = null, $value = null, string $boolean = 'and'): Builder {
-    //     if (!in_array($operator, ['!=', '=']) || func_num_args() === 2) {
-    //         $value = $operator;
-    //         $operator = '=';
-    //     }
-    //     $value = Crypt::encrypt($value);
-    //     return $this->where($column, $operator, $value, $boolean);
-    // });
+    /**
+     * where encrypted clause query to support encrypted columns
+     *
+     * @param  string $column
+     * @param  mixed  $operator = null
+     * @param  mixed  $value = null
+     * @param  string  $boolean = 'and'
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function whereEncrypted(): callable
+    {
+        return function (string $column, $operator = null, $value = null, string $boolean = 'and'): Builder {
+            if (!in_array($operator, ['!=', '=']) || func_num_args() === 2) {
+                $value = $operator;
+                $operator = '=';
+            }
+            $value = Crypt::encrypt($value);
+            return $this->where($column, $operator, $value, $boolean);
+        };
+    }
+    /**
+     * or where encrypted clause query to support encrypted columns
+     *
+     * @param  string  $column
+     * @param  mixed  $operator = null
+     * @param  mixed  $value = null
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function orWhereEncrypted(): callable
+    {
+        return function (string $column, $operator = null, $value = null): Builder {
+            return $this->whereEncrypted($column, $operator, $value, 'or');
+        };
+    }
 
-    // /**
-    //  * or where encrypted clause query to support encrypted columns
-    //  *
-    //  * @param  string  $column
-    //  * @param  mixed  $operator = null
-    //  * @param  mixed  $value = null
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('orWhereEncrypted', function (string $column, $operator = null, $value = null): Builder {
-    //     return $this->whereEncrypted($column, $operator, $value, 'or');
-    // });
+    /**
+     * where encrypted in clause query to support encrypted columns
+     *
+     * @param  string  $column
+     * @param  array  $values
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function whereEncryptedIn(): callable
+    {
+        return function (string $column, array $values): Builder {
+            $values = array_map(function ($value) {
+                return Crypt::encrypt($value);
+            }, $values);
+            return $this->whereIn($column, $values);
+        };
+    }
 
-    // /**
-    //  * where encrypted in clause query to support encrypted columns
-    //  *
-    //  * @param  string  $column
-    //  * @param  array  $values
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('whereEncryptedIn', function (string $column, array $values): Builder {
-    //     $values = array_map(function ($value) {
-    //         return Crypt::encrypt($value);
-    //     }, $values);
-    //     return $this->whereIn($column, $values);
-    // });
+    /**
+     * or where encrypted in clause query to support encrypted columns
+     *
+     * @param  string  $column
+     * @param  array  $values
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function orWhereEncryptedIn(): callable
+    {
+        return function (string $column, array $values): Builder {
+            $values = array_map(function ($value) {
+                return Crypt::encrypt($value);
+            }, $values);
 
-    // /**
-    //  * or where encrypted in clause query to support encrypted columns
-    //  *
-    //  * @param  string  $column
-    //  * @param  array  $values
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('orWhereEncryptedIn', function (string $column, array $values): Builder {
-    //     $values = array_map(function ($value) {
-    //         return Crypt::encrypt($value);
-    //     }, $values);
+            return $this->orWhereIn($column, $values);
+        };
+    }
 
-    //     return $this->orWhereIn($column, $values);
-    // });
+    /**
+     * where encrypted not in clause query to support encrypted columns
+     *
+     * @param  string  $column
+     * @param  array  $values
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function whereEncryptedNotIn(): callable
+    {
+        return function (string $column, array $values): Builder {
+            $values = array_map(function ($value) {
+                return Crypt::encrypt($value);
+            }, $values);
+            return $this->whereNotIn($column, $values);
+        };
+    }
 
-    // /**
-    //  * where encrypted not in clause query to support encrypted columns
-    //  *
-    //  * @param  string  $column
-    //  * @param  array  $values
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('whereEncryptedNotIn', function (string $column, array $values): Builder {
-    //     $values = array_map(function ($value) {
-    //         return Crypt::encrypt($value);
-    //     }, $values);
-    //     return $this->whereNotIn($column, $values);
-    // });
+    /**
+     * or where encrypted not in clause query to support encrypted columns
+     *
+     * @param  string  $column
+     * @param  array  $values
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function orWhereEncryptedNotIn(): callable
+    {
+        return function (string $column, array $values): Builder {
+            $values = array_map(function ($value) {
+                return Crypt::encrypt($value);
+            }, $values);
 
-    // /**
-    //  * or where encrypted not in clause query to support encrypted columns
-    //  *
-    //  * @param  string  $column
-    //  * @param  array  $values
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('orWhereEncryptedNotIn', function (string $column, array $values): Builder {
-    //     $values = array_map(function ($value) {
-    //         return Crypt::encrypt($value);
-    //     }, $values);
+            return $this->orWhereNotIn($column, $values);
+        };
+    }
 
-    //     return $this->orWhereNotIn($column, $values);
-    // });
+    /**
+     * where encrypted relation clause query to support encrypted columns
+     *
+     * @param  string  $relation
+     * @param  string  $column
+     * @param  mixed  $operator = null
+     * @param  mixed  $value = null
+     * 
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function whereEncryptedRelation(): callable
+    {
+        return function (string $relation, string $column, $operator = null, $value = null): Builder {
+            $value = Crypt::encrypt($value);
+            return $this->whereRelation($relation, $column, $operator, $value);
+        };
+    }
 
-    // /**
-    //  * where encrypted relation clause query to support encrypted columns
-    //  *
-    //  * @param  string  $relation
-    //  * @param  string  $column
-    //  * @param  mixed  $operator = null
-    //  * @param  mixed  $value = null
-    //  * 
-    //  * @return \Illuminate\Database\Eloquent\Builder
-    //  */
-    // Builder::macro('whereEncryptedRelation', function ($relation, $column, $operator = null, $value = null): Builder {
-    //     $value = Crypt::encrypt($value);
-    //     return $this->whereRelation($relation, $column, $operator, $value);
-    // });
-
-    // /**
-    //  * with media clause query that allows eager loading of media collections.
-    //  *
-    //  * @param mixed ...$collectionName The names of the media collections to be loaded.
-    //  * @return \Illuminate\Database\Eloquent\Builder The modified Builder instance.
-    //  */
-    // Builder::macro('withMedia', function (...$collectionName): Builder {
-    //     return $this->with([
-    //         'media' => fn ($query) => $query->wherePivotIn('collection_name', $collectionName),
-    //     ]);
-    // });
+    /**
+     * with media clause query that allows eager loading of media collections.
+     *
+     * @param mixed ...$collectionName The names of the media collections to be loaded.
+     * @return \Illuminate\Database\Eloquent\Builder The modified Builder instance.
+     */
+    public function withMedia(): callable
+    {
+        return function (...$collectionName): Builder {
+            return $this->with([
+                'media' => fn ($query) => $query->wherePivotIn('collection_name', $collectionName),
+            ]);
+        };
+    }
 }
