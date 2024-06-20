1d8c3f06603ad8750ef6fd5373e832948c47a809
sallieeky <sallieeky@gmail.com>
2024-06-20 21:48:23 +0800

    🎨 style(app.blade.php): add line-height property to improve readability of email content 🎨 style(footer.blade.php): add comments to indicate the start and end of the application logo section 🎨 style(header.blade.php): add comments to indicate the start and end of the application logo section 🎨 style(button.blade.php): add margin-top property to create spacing between button and previous element 🎨 style(subtitle.blade.php): remove margin-bottom property to improve spacing consistency 🎨 style(title.blade.php): remove margin-bottom property to improve spacing consistency

--- mail-stubs/resources/views/components/mails/layouts/header.blade.php
+++ mail-stubs/resources/views/components/mails/layouts/header.blade.php
@@ -1,3 +1,5 @@
 <div style="text-align: center">
-    <img src="https://itleaps.com/images/logo-long.png" alt="{{ config('app.name') }}" style="width: 50vw; height:50px; object-fit: contain; margin-top: 12px; margin-bottom: 12px">
+    {{-- Your Application Logo --}}
+    <img src="https://itleaps.com/images/logo-long.png" alt="{{ config('app.name') }}" style="width: 50vw; height:50px; object-fit: contain; margin-top: 24px; margin-bottom: 12px">
+    {{-- End Your Application Logo --}}
 </div>
--- mail-stubs/resources/views/components/mails/text/button.blade.php
+++ mail-stubs/resources/views/components/mails/text/button.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="{{ $url }}" style="background-color: #1268B3; color: #ffffff; padding: 8px; text-decoration: none; border-radius: 4px; display: block; text-align: center; margin: 8px 0">
+    <a href="{{ $url }}" style="background-color: #1268B3; color: #ffffff; padding: 8px; text-decoration: none; border-radius: 4px; display: block; text-align: center; margin-top: 16px">
         {{ $slot }}
     </a>
 </div>
