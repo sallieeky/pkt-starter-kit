1498673a41645acd47b87b9d1f68d762f8e438cc
sallieeky <sallieeky@gmail.com>
2024-06-24 11:04:31 +0800

    🔧 chore(mail.blade.php): improve readability and maintainability by adding comments to slots in mail template

--- mail-stubs/resources/views/mails/mail.blade.php
+++ mail-stubs/resources/views/mails/mail.blade.php
@@ -1,12 +1,15 @@
 <x-mails.layouts.app>
-    {{-- Optional Title --}}
-    <x-slot:title>{{-- Text --}}</x-slot:title>
+    <x-slot:title>
+        {{-- Optional Title --}}
+    </x-slot:title>
     
-    {{-- Optional Subtitle --}}
-    <x-slot:subtitle>{{-- Text --}}</x-slot:subtitle>
+    <x-slot:subtitle>
+        {{-- Optional Subtitle --}}
+    </x-slot:subtitle>
 
     {{-- Content --}}
 
-    {{-- Optional Button --}}
-    <x-slot:button url="#">{{-- Text --}}</x-slot:button>
+    <x-slot:button url="#">
+        {{-- Optional Button --}}
+    </x-slot:button>
 </x-mails.layouts.app>
