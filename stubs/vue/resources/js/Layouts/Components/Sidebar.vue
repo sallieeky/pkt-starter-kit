e0e91f57f35514eda90ffbd653b10871d6ea1227
sallieeky <sallieeky@gmail.com>
2024-07-17 10:01:48 +0800

    🔧 chore(GUIDE.md): add new menu items for Master Data, Personalization, Notification, and Setting to improve navigation options 🐛 fix(Sidebar.vue): adjust sidebar height to accommodate fixed menu items and improve scrolling experience 🔧 chore(Sidebar.vue): filter out fixed menu items from sideMenuItems and create separate fixedMenuItems array for improved rendering and styling

--- stubs/vue/resources/js/Layouts/Components/Sidebar.vue
+++ stubs/vue/resources/js/Layouts/Components/Sidebar.vue
@@ -1,37 +1,77 @@
 <template>
-    <div class="hidden lg:block p-4 fixed h-screen pb-24 max-h-screen mt-20 overflow-scroll w-20 no-scrollbar transition-[width] 
+    <div class="hidden lg:block p-4 h-screen fixed pb-24 mt-20 overflow-hidden w-20 no-scrollbar transition-[width]
                 duration-300 ease-in-out bg-[url('/images/pkt-pattern.png')] bg-contain bg-left-bottom bg-no-repeat bg-primary-surface z-50"
         :class="[
             { 'w-20': sidemenu },
             { 'w-64': !sidemenu }
         ]">
-        <nav class="flex-grow">
+        <nav class="flex-grow overflow-auto no-scrollbar" :class="[
+            { 'h-[60vh]': fixedMenuItems.length },
+            { 'h-screen pb-24': !fixedMenuItems.length }
+        ]">
             <ul class="my-2 flex flex-col gap-2 items-stretch">
                 <li v-for="menuItem, index in sideMenuItems" :key="index">
-                    <sidebar-menu-item v-if="can(menuItem.permission)" :menu-item="menuItem" :key="index">
+                    <div v-if="can(menuItem.permission) && menuItem.type === 'header' && !sidemenu" class="text-xs text-gray-700 uppercase font-bold mb-2">
+                        {{ menuItem.label }}
+                    </div>
+                    <sidebar-menu-item v-if="can(menuItem.permission) && menuItem.type !== 'header'" :menu-item="menuItem" :key="index">
                         {{ menuItem.permission }}
                     </sidebar-menu-item>
                 </li>
             </ul>
         </nav>
+
+        <div v-if="fixedMenuItems.length">
+            <nav class="flex-grow max-h-[30vh] overflow-auto no-scrollbar border-t-2 border-gray-300">
+                <ul class="my-2 flex flex-col gap-2 items-stretch">
+                    <li v-for="menuItem, index in fixedMenuItems" :key="index">
+                        <div v-if="can(menuItem.permission) && menuItem.type === 'fixed-header' && !sidemenu" class="text-xs text-gray-700 uppercase font-bold mb-2">
+                            {{ menuItem.label }}
+                        </div>
+                        <sidebar-menu-item v-if="can(menuItem.permission) && menuItem.type !== 'fixed-header'" :menu-item="menuItem" :key="index">
+                            {{ menuItem.permission }}
+                        </sidebar-menu-item>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-    <div class="block lg:hidden p-4 fixed h-screen max-h-screen mt-20 overflow-scroll w-64 no-scrollbar transition-all shadow-xl
+    <div class="block lg:hidden p-4 fixed h-screen mt-20 overflow-hidden w-64 no-scrollbar transition-all shadow-xl
                 duration-300 ease-in-out bg-[url('/images/pkt-pattern.png')] bg-contain bg-left-bottom bg-no-repeat bg-primary-surface z-50"
         :class="[
             { '-left-64': !drawer },
             { 'left-0': drawer }
         ]">
-        <nav class="flex-grow">
+        <nav class="flex-grow overflow-auto no-scrollbar" :class="[
+            { 'h-[60vh]': fixedMenuItems.length },
+            { 'h-screen pb-24': !fixedMenuItems.length }
+        ]">
             <ul class="my-2 flex flex-col gap-2 items-stretch">
                 <li v-for="menuItem, index in sideMenuItems" :key="index">
-                    <sidebar-menu-item v-if="can(menuItem.permission)" :menu-item="menuItem" :unique="index">
+                    <div v-if="can(menuItem.permission) && menuItem.type === 'header' && !sidemenu" class="text-xs text-gray-700 uppercase font-bold mb-2">
+                        {{ menuItem.label }}
+                    </div>
+                    <sidebar-menu-item v-if="can(menuItem.permission) && menuItem.type !== 'header'" :menu-item="menuItem" :key="index">
                         {{ menuItem.permission }}
                     </sidebar-menu-item>
                 </li>
             </ul>
         </nav>
+        <div v-if="fixedMenuItems.length">
+            <nav class="flex-grow max-h-[30vh] overflow-auto no-scrollbar border-t-2 border-gray-300">
+                <ul class="my-2 flex flex-col gap-2 items-stretch">
+                    <li v-for="menuItem, index in fixedMenuItems" :key="index">
+                        <div v-if="can(menuItem.permission) && menuItem.type === 'fixed-header' && !sidemenu" class="text-xs text-gray-700 uppercase font-bold mb-2">
+                            {{ menuItem.label }}
+                        </div>
+                        <sidebar-menu-item v-if="can(menuItem.permission) && menuItem.type !== 'fixed-header'" :menu-item="menuItem" :key="index">
+                            {{ menuItem.permission }}
+                        </sidebar-menu-item>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-    <div class="block lg:hidden bg-black"></div>
 </template>
 
 <script setup>
@@ -46,5 +86,6 @@ const sideMenuStore = useSidemenuStore();
 const sidemenu = computed(()=>sideMenuStore.sidemenu);
 const drawer = computed(()=>sideMenuStore.drawer);
 
-const sideMenuItems = ref(navItems);
+const sideMenuItems = ref(navItems.filter(item => !item.type?.includes('fixed')));
+const fixedMenuItems = ref(navItems.filter(item => item.type?.includes('fixed')));
 </script>
