49e7bba0f5bdc06726dd5eb89467b7ae7551858d
Muhammad Ardhian <muhamad.ardian24@gmail.com>
2024-07-18 08:33:54 +0800

    ✨ (images/logo-long-small.png): add new logo image for the application ✨ (style/splash-screen.css): add new CSS file for styling the splash screen ✨ (app.js): update progress configuration for the app to include delay, color, and showSpinner properties 📝 (app.blade.php): add link to the splash-screen.css file and include loading markup for the splash screen in the body of the page

--- stubs/vue/resources/js/app.js
+++ stubs/vue/resources/js/app.js
@@ -14,14 +14,17 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    progress: {
+        delay: 250,
+        color: '#0a4f86',
+        showSpinner: false,
+    },
     setup({ el, App, props, plugin }) {
+        document.body.classList.remove('page-loading');
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(createPinia())
             .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
     },
 });
