796757b7057b6df286b5b7b6b28226f135050625
sallieeky <sallieeky@gmail.com>
2024-07-19 21:57:13 +0800

    🔧 fix(AccountController.php): use caching to improve performance when retrieving account picture
    🔒 refactor(AccountController.php): use Laravel's Cache facade to cache the account picture for 30 days, reducing the number of HTTP requests made to retrieve the picture

--- stubs/default/app/Http/Controllers/Starter/AccountController.php
+++ stubs/default/app/Http/Controllers/Starter/AccountController.php
@@ -4,20 +4,25 @@ namespace App\Http\Controllers\Starter;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 
 class AccountController extends Controller
 {
-    public function accountPicture($npk) 
+    public function accountPicture($npk)
     {
         try {
-            $request = Http::get("https://leader.pupukkaltim.com/public/foto-thumbnail/$npk.jpg");
-            $picture = $request->body();
-            $status = $request->status();
-            if ($status !== 200) {
-                $picture = file_get_contents(public_path('/images/avatar-default.png'));
-            }
+            $cacheDuration = now()->addDays(30);
+            $picture = Cache::remember("account-picture-$npk", $cacheDuration, function () use ($npk) {
+                $request = Http::get("https://leader.pupukkaltim.com/public/foto-thumbnail/$npk.jpg");
+                $picture = $request->body();
+                $status = $request->status();
+                if ($status !== 200) {
+                    $picture = file_get_contents(public_path('/images/avatar-default.png'));
+                }
+                return $picture;
+            });
         } catch (\Exception $e) {
             $picture = file_get_contents(public_path('/images/avatar-default.png'));
         }
